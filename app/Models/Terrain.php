570fdeb11f7c1ba0012cb5d56def1ccae2e65c89
tongsour <tongsour1000@gmail.com>
2025-06-25 14:15:03 +0700

    final-devops

---
+++ app/Models/Terrain.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Terrain extends Model
+{
+    /** @use HasFactory<\Database\Factories\TerrainFactory> */
+    use HasFactory;
+}
